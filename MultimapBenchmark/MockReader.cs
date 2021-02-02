5d7941df14096afb2162f8dc75bdcea428cdcbfd
azyobuzin <azyobuzin@users.sourceforge.jp>
2021-02-02 21:51:54 +0900

    Initial commit

---
+++ MultimapBenchmark/MockReader.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Data.Common;
+
+namespace MultimapBenchmark
+{
+    public sealed class MockReader : DbDataReader
+    {
+        private int _columns;
+        private int _rows;
+        private long _value;
+
+        public MockReader(int columns, int rows)
+        {
+            _columns = columns;
+            _rows = rows;
+        }
+
+        public override object this[int ordinal] => GetInt64(ordinal);
+
+        public override object this[string name] => name == "Id" ? _value : throw new IndexOutOfRangeException();
+
+        public override int Depth => 0;
+
+        public override int FieldCount => _columns;
+
+        public override bool HasRows => _rows > 0;
+
+        public override bool IsClosed => false;
+
+        public override int RecordsAffected => 0;
+
+        public override bool GetBoolean(int ordinal) => Convert.ToBoolean(GetInt64(ordinal));
+
+        public override byte GetByte(int ordinal) => Convert.ToByte(GetInt64(ordinal));
+
+        public override long GetBytes(int ordinal, long dataOffset, byte[] buffer, int bufferOffset, int length)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override char GetChar(int ordinal) => Convert.ToChar(GetInt64(ordinal));
+
+        public override long GetChars(int ordinal, long dataOffset, char[] buffer, int bufferOffset, int length)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetDataTypeName(int ordinal) =>
+            (uint)ordinal < (uint)_columns ? "INT" : throw new IndexOutOfRangeException();
+
+        public override DateTime GetDateTime(int ordinal) => Convert.ToDateTime(GetInt64(ordinal));
+
+        public override decimal GetDecimal(int ordinal) => Convert.ToDecimal(GetInt64(ordinal));
+
+        public override double GetDouble(int ordinal) => Convert.ToDouble(GetInt64(ordinal));
+
+        public override IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Type GetFieldType(int ordinal) =>
+            (uint)ordinal < (uint)_columns ? typeof(long) : throw new IndexOutOfRangeException();
+
+        public override float GetFloat(int ordinal) => Convert.ToSingle(GetInt64(ordinal));
+
+        public override Guid GetGuid(int ordinal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override short GetInt16(int ordinal) => Convert.ToInt16(GetInt64(ordinal));
+
+        public override int GetInt32(int ordinal) => Convert.ToInt32(GetInt64(ordinal));
+
+        public override long GetInt64(int ordinal) => (uint)ordinal < (uint)_columns ? _value : throw new IndexOutOfRangeException();
+
+        public override string GetName(int ordinal) => (uint)ordinal < (uint)_columns ? "Id" : throw new IndexOutOfRangeException();
+
+        public override int GetOrdinal(string name) => name == "Id" ? 0 : throw new IndexOutOfRangeException();
+
+        public override string GetString(int ordinal) => Convert.ToString(GetInt64(ordinal));
+
+        public override object GetValue(int ordinal) => GetInt64(ordinal);
+
+        public override int GetValues(object[] values)
+        {
+            var count = Math.Min(_columns, values.Length);
+            for (var i = 0; i < count; i++) values[i] = _value;
+            return count;
+        }
+
+        public override bool IsDBNull(int ordinal) => (uint)ordinal < (uint)_columns ? false : throw new IndexOutOfRangeException();
+
+        public override bool NextResult()
+        {
+            _rows = 0;
+            return false;
+        }
+
+        public override bool Read()
+        {
+            if (_value >= _rows) return false;
+
+            _value++;
+            return true;
+        }
+    }
+}
--- MultimapBenchmark/Program.cs
+++ MultimapBenchmark/Program.cs
@@ -1,12 +1,118 @@
-﻿using System;
+﻿extern alias DapperBaseline;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+using Moq;
 
 namespace MultimapBenchmark
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BenchmarkRunner.Run<Program>(Debugger.IsAttached ? new DebugInProcessConfig() : null);
+        }
+
+        private const string Sql = "SELECT * FROM TestTable";
+
+        [Params(100)]
+        public int Rows { get; set; }
+
+        private IDbConnection _connection1;
+        private IDbConnection _connection2;
+        private IDbConnection _connection3;
+        private IDbConnection _connection5;
+        private IDbConnection _connection7;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _connection1 = CreateMockConnection(1, Rows);
+            _connection2 = CreateMockConnection(2, Rows);
+            _connection3 = CreateMockConnection(3, Rows);
+            _connection5 = CreateMockConnection(5, Rows);
+            _connection7 = CreateMockConnection(7, Rows);
+        }
+
+        private static IDbConnection CreateMockConnection(int columns, int rows)
+        {
+            var commandMock = new Mock<IDbCommand>();
+            commandMock.SetupProperty(x => x.CommandText);
+            commandMock.Setup(x => x.ExecuteReader(It.IsAny<CommandBehavior>()))
+                .Returns(() => new MockReader(columns, rows));
+            var command = commandMock.Object;
+
+            var connectionMock = new Mock<IDbConnection>();
+            connectionMock.Setup(x => x.State).Returns(ConnectionState.Open);
+            connectionMock.Setup(x => x.CreateCommand()).Returns(command);
+
+            return connectionMock.Object;
+        }
+
+        [Benchmark]
+        public List<long> QueryLong()
+        {
+            var results = DapperBaseline::Dapper.SqlMapper.Query<long>(_connection1, Sql);
+            return DapperBaseline::Dapper.SqlMapper.AsList(results);
+        }
+
+        [Benchmark]
+        public List<(long, long)> BaselineMultiMap2()
+        {
+            var results = DapperBaseline::Dapper.SqlMapper.Query(_connection2, Sql, (long a, long b) => (a, b));
+            return DapperBaseline::Dapper.SqlMapper.AsList(results);
+        }
+
+        [Benchmark]
+        public List<(long, long, long)> BaselineMultiMap3()
+        {
+            var results = DapperBaseline::Dapper.SqlMapper.Query(_connection3, Sql, (long a, long b, long c) => (a, b, c));
+            return DapperBaseline::Dapper.SqlMapper.AsList(results);
+        }
+
+        [Benchmark]
+        public List<(long, long, long, long, long)> BaselineMultiMap5()
+        {
+            var results = DapperBaseline::Dapper.SqlMapper.Query(_connection5, Sql, (long a, long b, long c, long d, long e) => (a, b, c, d, e));
+            return DapperBaseline::Dapper.SqlMapper.AsList(results);
+        }
+
+        [Benchmark]
+        public List<(long, long, long, long, long, long, long)> BaselineMultiMap7()
+        {
+            var results = DapperBaseline::Dapper.SqlMapper.Query(_connection7, Sql, (long a, long b, long c, long d, long e, long f, long g) => (a, b, c, d, e, f, g));
+            return DapperBaseline::Dapper.SqlMapper.AsList(results);
+        }
+
+        [Benchmark]
+        public List<(long, long)> FixedMultiMap2()
+        {
+            var results = Dapper.SqlMapper.Query(_connection2, Sql, (long a, long b) => (a, b));
+            return Dapper.SqlMapper.AsList(results);
+        }
+
+        [Benchmark]
+        public List<(long, long, long)> FixedMultiMap3()
+        {
+            var results = Dapper.SqlMapper.Query(_connection3, Sql, (long a, long b, long c) => (a, b, c));
+            return Dapper.SqlMapper.AsList(results);
+        }
+
+        [Benchmark]
+        public List<(long, long, long, long, long)> FixedMultiMap5()
+        {
+            var results = Dapper.SqlMapper.Query(_connection5, Sql, (long a, long b, long c, long d, long e) => (a, b, c, d, e));
+            return Dapper.SqlMapper.AsList(results);
+        }
+
+        [Benchmark]
+        public List<(long, long, long, long, long, long, long)> FixedMultiMap7()
+        {
+            var results = Dapper.SqlMapper.Query(_connection7, Sql, (long a, long b, long c, long d, long e, long f, long g) => (a, b, c, d, e, f, g));
+            return Dapper.SqlMapper.AsList(results);
         }
     }
 }
