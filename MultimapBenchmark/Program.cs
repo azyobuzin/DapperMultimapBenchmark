b2955e9c1b2702dd7b79c0f53114a4014602dc84
azyobuzin <azyobuzin@users.sourceforge.jp>
2021-02-02 19:48:13 +0900

    プロジェクト ファイルを追加します。

---
+++ MultimapBenchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MultimapBenchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
